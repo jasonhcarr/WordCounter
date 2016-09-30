979dbf156d0d86231c140dd3200441ae346b76f8
jasonhcarr <jasonhcarr@gmail.com>
2016-09-30 16:02:35 -0400

    final epic and legendary

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,4 @@
-// (function(){
-  // this.wordCounter.splitter();
-var sonnetText = "Shall I compare thee to a summer's day? Thou art more lovely and more temperate: Rough winds do shake the darling buds of May, And summer's lease hath all too short a date: Sometime too hot the eye of heaven shines, And often is his gold complexion dimmed, And every fair from fair sometime declines, By chance, or nature's changing course untrimmed:  But thy eternal summer shall not fade, Nor lose possession of that fair thou ow'st, Nor shall death brag thou wand'rest in his shade, When in eternal lines to time thou grow'st, So long as men can breathe or eyes can see,So long lives this, and this gives life to thee.";
+
 
 var wordCounter = {
 
@@ -8,11 +6,13 @@ var wordCounter = {
         splitText: null,
         splitTextReduced: null,
         textCollated: null,
-        sortedText: null
+        sortedText: null,
+        arrangedText: null
     },
 
 
-    splitter: function() {
+    splitter: function(sonnet1, sonnet2) {
+        var sonnetText = sonnet1 + " " + sonnet2;
         var lowerCase= sonnetText.toLowerCase();
         var splitText = lowerCase.match(/\w+/g);
         this.values.splitText = splitText;
@@ -51,9 +51,10 @@ var wordCounter = {
         }
       );
       this.values.sortedText = sortedText;
-      
+      var arrangedText = sortedText.join('\n');
+      this.values.arrangedText = arrangedText;
     }
 };
 
-wordCounter.splitter();
-console.log(wordCounter.values.sortedText);
+wordCounter.splitter("Shall I compare thee to a summer's day? Thou art more lovely and more temperate: Rough winds do shake the darling buds of May, And summer's lease hath all too short a date: Sometime too hot the eye of heaven shines, And often is his gold complexion dimmed, And every fair from fair sometime declines, By chance, or nature's changing course untrimmed:  But thy eternal summer shall not fade, Nor lose possession of that fair thou ow'st, Nor shall death brag thou wand'rest in his shade, When in eternal lines to time thou grow'st, So long as men can breathe or eyes can see,So long lives this, and this gives life to thee.", "Let me not to the marriage of true minds Admit impediments. Love is not love Which alters when it alteration finds, Or bends with the remover to remove: O, no! it is an ever-fixed mark, That looks on tempests and is never shaken; It is the star to every wandering bark, Whose worth's unknown, although his height be taken. Love's not Time's fool, though rosy lips and cheeks Within his bending sickle's compass come; Love alters not with his brief hours and weeks, But bears it out even to the edge of doom. If this be error and upon me proved, I never writ, nor no man ever loved.");
+console.log(wordCounter.values.arrangedText);
